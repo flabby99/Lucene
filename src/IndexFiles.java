a0e4711d77340bdcd663fc2936e13a9debf7f392
Sean Martin <seankieran.m@hotmail.com>
2018-02-18 12:48:45 +0000

    Initial Indexing attempt of cranfield

--- src/IndexFiles.java
+++ src/IndexFiles.java
@@ -1,5 +1,5 @@
 /*
- * SEAN MARTIN 
+ * SEAN MARTIN
  * This code is based on the Apache Lucene demo Indexing file
  * Found at https://lucene.apache.org/core/7_2_1/demo/src-html/org/apache/lucene/demo/IndexFiles.html
  */
@@ -8,8 +8,6 @@ package indexing;
 
 import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.FileVisitResult;
 import java.nio.file.Files;
@@ -18,14 +16,16 @@ import java.nio.file.Paths;
 import java.nio.file.SimpleFileVisitor;
 import java.nio.file.attribute.BasicFileAttributes;
 import java.util.Date;
+import java.lang.StringBuilder;
 
 import org.apache.lucene.analysis.Analyzer;
 import org.apache.lucene.analysis.standard.StandardAnalyzer;
-import org.apache.lucene.document.LongPoint;
+import org.apache.lucene.document.IntPoint;
 import org.apache.lucene.document.Document;
 import org.apache.lucene.document.Field;
 import org.apache.lucene.document.StringField;
 import org.apache.lucene.document.TextField;
+import org.apache.lucene.document.StoredField;
 import org.apache.lucene.index.IndexWriter;
 import org.apache.lucene.index.IndexWriterConfig.OpenMode;
 import org.apache.lucene.index.IndexWriterConfig;
@@ -93,13 +93,13 @@ public class IndexFiles {
 
       // Optional: for better indexing performance, if you
       // are indexing many documents, increase the RAM
-      // buffer.  But if you do this, increase the max heap	
+      // buffer.  But if you do this, increase the max heap
       // size to the JVM (eg add -Xmx512m or -Xmx1g):
       //
       // iwc.setRAMBufferSizeMB(256.0);
 
       IndexWriter writer = new IndexWriter(dir, iwc);
-      indexDocs(writer, docDir);
+      indexCranfield(writer, docDir);
 
       // NOTE: if you want to maximize search performance,
       // you can optionally call forceMerge here.  This can be
@@ -120,78 +120,76 @@ public class IndexFiles {
     }
   }
 
-  /**
-   * Indexes the given file using the given writer, or if a directory is given,
-   * recurses over files and directories found under the given directory.
-   *
-   * NOTE: This method indexes one document per input file.  This is slow.  For good
-   * throughput, put multiple documents into your input file(s).  An example of this is
-   * in the benchmark module, which can create "line doc" files, one document per line,
-   * using the
-   * <a href="../../../../../contrib-benchmark/org/apache/lucene/benchmark/byTask/tasks/WriteLineDocTask.html"
-   * >WriteLineDocTask</a>.
-   *
-   * @param writer Writer to the index where the given file/dir info will be stored
-   * @param path The file to index, or the directory to recurse into to find files to index
-   * @throws IOException If there is a low-level I/O error
-   */
-  static void indexDocs(final IndexWriter writer, Path path) throws IOException {
-    if (Files.isDirectory(path)) {
-      Files.walkFileTree(path, new SimpleFileVisitor<Path>() {
-        @Override
-        public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
-          try {
-            indexDoc(writer, file, attrs.lastModifiedTime().toMillis());
-          } catch (IOException ignore) {
-            // don't index files that can't be read.
-          }
-          return FileVisitResult.CONTINUE;
-        }
-      });
-    } else {
-      indexDoc(writer, path, Files.getLastModifiedTime(path).toMillis());
+  /** Reads through the cranfield collection and indexes it */
+  static void indexCranfield(IndexWriter writer, Path file) throws IOException {
+    String line = null;
+    StringBuilder stringBuilder = new StringBuilder();
+    int doc_index = 1;
+    Document doc;
+    try {
+         // Create a buffered Reader
+         BufferedReader bufferedReader =
+             new BufferedReader(file, StandardCharsets.UTF_8);
+         //Read until end of file
+         while((line = bufferedReader.readLine()) != null) {
+           switch (line) {
+             case ".I " + doc_index :
+              if (doc_index != 1) {
+                doc.add(new TextField("Words", stringBuilder.toString()));
+                stringBuilder.setLength(0);
+                indexDocument(writer, doc);
+              }
+              doc = new Document();
+              doc.add(new IntPoint("Instance_ID", doc_index));
+              doc.add(new StoredField("Instance_ID", doc_index));
+              ++doc_index;
+              break;
+
+            case ".T" :
+              stringBuilder.setLength(0);
+
+            case ".A" :
+            //TODO check if the default for this is to store or not store it
+              doc.add(new TextField("Title", stringBuilder.toString()));
+              stringBuilder.setLength(0);
+
+            case ".B" :
+              doc.add(new TextField("Abstract", stringBuilder.toString()));
+              stringBuilder.setLength(0);
+
+            case ".W" :
+              doc.add(new StringField("Bibliographic", stringBuilder.toString()));
+              stringBuilder.setLength(0);
+            case
+            default:
+              stringBuilder.append(line);
+           }
+         }
+
+         //close file.
+         bufferedReader.close();
+      }
+      catch(IOException ex) {
+         ex.printStackTrace();
+      }
     }
-  }
 
-  /** Indexes a single document */
-  static void indexDoc(IndexWriter writer, Path file, long lastModified) throws IOException {
-    try (InputStream stream = Files.newInputStream(file)) {
-      // make a new, empty document
-      Document doc = new Document();
-
-      // Add the path of the file as a field named "path".  Use a
-      // field that is indexed (i.e. searchable), but don't tokenize
-      // the field into separate words and don't index term frequency
-      // or positional information:
-      Field pathField = new StringField("path", file.toString(), Field.Store.YES);
-      doc.add(pathField);
-
-      // Add the last modified date of the file a field named "modified".
-      // Use a LongPoint that is indexed (i.e. efficiently filterable with
-      // PointRangeQuery).  This indexes to milli-second resolution, which
-      // is often too fine.  You could instead create a number based on
-      // year/month/day/hour/minutes/seconds, down the resolution you require.
-      // For example the long value 2011021714 would mean
-      // February 17, 2011, 2-3 PM.
-      doc.add(new LongPoint("modified", lastModified));
-
-      // Add the contents of the file to a field named "contents".  Specify a Reader,
-      // so that the text of the file is tokenized and indexed, but not stored.
-      // Note that FileReader expects the file to be in UTF-8 encoding.
-      // If that's not the case searching for special characters will fail.
-      doc.add(new TextField("contents", new BufferedReader(new InputStreamReader(stream, StandardCharsets.UTF_8))));
-
+    /** Indexes a single document using writer*/
+    static void indexDocument(IndexWriter writer, Document doc) {
       if (writer.getConfig().getOpenMode() == OpenMode.CREATE) {
         // New index, so we just add the document (no old document can be there):
+        //TODO update this print
         System.out.println("adding " + file);
         writer.addDocument(doc);
       } else {
         // Existing index (an old copy of this document may have been indexed) so
         // we use updateDocument instead to replace the old one matching the exact
         // path, if present:
+        //TODO update this print
         System.out.println("updating " + file);
         writer.updateDocument(new Term("path", file.toString()), doc);
       }
     }
+
   }
 }
